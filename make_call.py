7a1545e4b84d4be3ef11cd4e21e4b6780ca4a32b
Andrew Wright <Andy@localhost.localdomain>
2017-12-28 18:10:21 -0500

    Trying url_for for make_call function

--- make_call.py
+++ make_call.py
@@ -1,5 +1,6 @@
 import os, subprocess
 from twilio.rest import Client
+from flask import url_for
 
 
 def make_call(num):
@@ -8,6 +9,6 @@ def make_call(num):
     auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
     my_phone = os.environ.get('TWILIO_PHONE_NUMBER')
     client = Client(account_sid, auth_token)
-    client.calls.create(to=num, from_=my_phone, url='https://lendup-awright.herokuapp.com/incoming')
+    client.calls.create(to=num, from_=my_phone, url=url_for('incoming'))
 
 
